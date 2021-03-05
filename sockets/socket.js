d09d097aae1905f26f4c4b466977dad28b6787d8
root <root@ROBERT-ALVAREZ.localdomain>
2021-03-05 13:51:46 -0400

    mejoras de bands y socket

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,22 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Metallica'));
+bands.addBand(new Band('CowBoy'));
+bands.addBand(new Band('Good Mean'));
+bands.addBand(new Band('Day'));
+
+console.log(bands.getBands());
 
 io.on('connection', client => {
+    
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
+    
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
@@ -12,9 +27,24 @@ io.on('connection', client => {
         io.emit('broatcats', { admin: 'nuevo mensaje a todos los clientes' });
     });
 
-    client.on('mensaje-navegador-web', (payload) => {
+    client.on('emitir-mensaje', (payload) => {
         client.broadcast.emit('nuevo-mensaje', payload);
         console.log(payload);
+    });
+
+    client.on('vote-band', ( payload ) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', ( payload ) => {
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', ( payload ) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
     });
 
 });
